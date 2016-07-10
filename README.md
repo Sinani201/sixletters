5e0f4728c29e02b2b4be0c8ab7f572bbd117cb0b
Daniel Kessler <sinani201@gmail.com>
2016-07-10 16:30:05 -0700

    added more arguments to expand.py

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ https://dkess.me/sixletters ([backup link](https://www.ocf.berkeley.edu/~dkessle
 ## Pre-caching levels
 By default, to generate a level, clients will download the entire word list, pick a random 6 letter word, and fill in the remaining words based on anagrams. This process is slow and consumes excessive bandwidth. As an alternative, the `expand.py` python script can be run to pre-cache each level in the `levels` directory. This process generates 1069 levels, consumes 4.3M of disk space, and takes ~30 seconds when run with PyPy.
 
+You can also set extra options when pre-caching levels. Run `expand.py --help` to see all of them. For example, if you are interested in excluding obscene words, you can download [this list](https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/blob/master/en) and use the file as an argument.
+
+```
+python expand.py -b <(curl https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en)
+```
+
+
 ## Multiplayer
 You can play SixLetters cooperatively with your friends! Just press the "Share Game" button to host, and share the link. The server is written in Python and its [source](https://github.com/dkess/sl_gameserver) is on GitHub. Competitive multiplayer is coming soon!
--- expand.py
+++ expand.py
@@ -1,14 +1,34 @@
 from collections import Counter
 import fileinput
+import argparse
 import os
 
-MIN_WORDS = 23
+parser = argparse.ArgumentParser(
+    description='Pre-caches every SixLetters level.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-w', '--wordsfile', default='words.txt', type=open,
+                    help='The file with words to use')
+parser.add_argument('-l', '--levelsdir', default='levels',
+                    help=('The directory to store the levels in. Will be created'
+                          ' if it does not exist already.'))
+parser.add_argument('-c', '--min-words', default=23, type=int,
+                    help='The minimum amount of words a level can have.')
+parser.add_argument('-b', '--badwords', type=open,
+                    help='A list of bad words to exclude from all levels')
 
-DIR_NAME = 'levels'
+args = parser.parse_args()
 
-f = open('words.txt')
-words = [l.rstrip() for l in f]
+MIN_WORDS = args.min_words
+DIR_NAME = args.levelsdir
+
+f = args.wordsfile
+words = [l.rstrip().lower() for l in f]
 f.close()
+
+badwords = set()
+if args.badwords:
+    badwords = set(w.rstrip().lower() for w in args.badwords
+                   if 3 <= len(w.rstrip()) <= 6 and w.rstrip() in words)
 
 counter = 0
 
@@ -20,6 +40,8 @@ for lw in words:
         level_count = 0
         level_words = ''
         for w in words:
+            if w in badwords:
+                continue
             lw_c = Counter(lw)
             w_c = Counter(w)
             valid_anagram = True
@@ -34,3 +56,6 @@ for lw in words:
             with open('{}/{:0>4}.txt'.format(DIR_NAME, counter), 'w') as f:
                 f.write(level_words)
             counter += 1
+
+print('// Put this in your config.js file:')
+print('LEVELS_COUNT="{}";'.format(counter))
