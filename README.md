f43cb5a634eebb013d57662d5e9f5011f9ea793d
Daniel Kessler <sinani201@gmail.com>
2016-07-19 21:39:30 -0700

    added Rust version of expand script

--- README.md
+++ README.md
@@ -4,14 +4,9 @@ attempt to form as many words as possible.  A live demo is available at
 https://dkess.me/sixletters ([backup link](https://www.ocf.berkeley.edu/~dkessler/sixletters/)) ([backup link 2](https://dkess.github.io/sixletters/)).
 
 ## Pre-caching levels
-By default, to generate a level, clients will download the entire word list, pick a random 6 letter word, and fill in the remaining words based on anagrams. This process is slow and consumes excessive bandwidth. As an alternative, the `expand.py` python script can be run to pre-cache each level in the `levels` directory. This process generates 1206 levels, consumes 4.3M of disk space, and takes ~30 seconds when run with PyPy.
+By default, clients must download the entire word list on each page load to generate a level. This process is slow and consumes excessive bandwidth. As an alternative, the `expand.py` python script can be run to pre-cache each level in the `levels` directory. This process generates 1206 levels, consumes 4.3M of disk space, and takes ~30 seconds when run with PyPy or 3-4 minutes when run with CPython. If this is too slow, you can use the alternate Rust version, which runs in less than a second: `rustc -O expand.rs && ./expand`.
 
-You can also set extra options when pre-caching levels. Run `expand.py --help` to see all of them. For example, if you are interested in excluding obscene words, you can download [this list](https://github.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/blob/master/en) and use the file as an argument.
-
-```
-python expand.py -b <(curl https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en)
-```
-
+Pre-caching levels also allows for extra options. You can blacklist obscene words, set the minimum amount of words per level (to prevent very small levels from appearing). Run `expand.py --help` to see all options. Both expand.py ad expand.rs take the same options.
 
 ## Multiplayer
 You can play SixLetters cooperatively with your friends! Just press the "Share Game" button to host, and share the link. The server is written in Python and its [source](https://github.com/dkess/sl_gameserver) is on GitHub. Competitive multiplayer is coming soon!
---
+++ expand.rs
@@ -0,0 +1,167 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+use std::path::Path;
+
+struct WordData {
+    word: String,
+    sorted: String
+}
+
+/// Checks if the letters of small can be formed from the letters of big.
+/// Both arguments should be sorted.
+fn is_subgram(small: &String, big: &String) -> bool {
+    let mut big_iter = big.bytes();
+    let mut big_on = big_iter.next().unwrap();
+
+    let mut small_iter = small.bytes();
+    let mut small_on = small_iter.next().unwrap();
+
+    loop {
+        if small_on < big_on {
+            return false;
+        } else if small_on == big_on {
+            small_on = match small_iter.next() {
+                Some(x) => x,
+                None => return true,
+            };
+        }
+        big_on = match big_iter.next() {
+            Some(x) => x,
+            None => ('z' as u8) + 1,
+        };
+    }
+}
+
+fn main() {
+    // Parse command-line args
+    let mut last_arg = String::from("");
+
+    // defaults
+    let mut wordsfile = String::from("words.txt");
+    let mut levelsdir = String::from("levels");
+    let mut min_words = 23;
+    let mut badwordsfile = String::from("");
+    let mut dryrun = false;
+
+    for arg in env::args().skip(1) {
+        if last_arg == "" {
+            if arg == "--dry-run" {
+                dryrun = true;
+            } if arg == "-h" || arg == "--help" {
+                println!(
+r#"usage: {} [-h] [-w WORDSFILE] [-l LEVELSDIR] [-c MIN_WORDS]
+                 [-b BADWORDS] [--dry-run]
+
+Pre-caches every SixLetters level.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -w WORDSFILE, --wordsfile WORDSFILE
+                        The file with words to use (default: 23)
+  -l LEVELSDIR, --levelsdir LEVELSDIR
+                        The directory to store the levels in. Will be created
+                        if it does not exist already. (default: levels/)
+  -c MIN_WORDS, --min-words MIN_WORDS
+                        The minimum amount of words a level can have.
+                        (default: 23)
+  -b BADWORDS, --badwords BADWORDS
+                        A list of bad words to exclude from all levels
+                        (default: None)
+  --dry-run             Do not actually write to filesystem (default: False)"#,
+                    env::args().nth(0).unwrap());
+                return;
+            } else {
+                last_arg = arg;
+            }
+        } else {
+            if last_arg == "-w" || last_arg == "--wordsfile" {
+                wordsfile = arg;
+            } else if last_arg == "-l" || last_arg == "--levelsdir" {
+                levelsdir = arg;
+            } else if last_arg == "-c" || last_arg == "--min-words" {
+                min_words = arg.parse::<usize>().unwrap();
+            } else if last_arg == "-b" || last_arg == "--badwords" {
+                badwordsfile = arg;
+            }
+            last_arg = String::from("");
+        }
+    }
+
+    let badwords = if badwordsfile != "" {
+        let bwf = File::open(badwordsfile).unwrap();
+        let bwf = BufReader::new(bwf);
+
+        bwf.lines()
+            .filter_map(|x| {
+                let x = x.unwrap();
+                if x.len() <= 6 {
+                    Some(x.to_lowercase())
+                } else {
+                    None
+                }})
+            .collect::<HashSet<String>>()
+    } else {
+        HashSet::new()
+    };
+
+    let f = File::open(wordsfile).unwrap();
+    let f = BufReader::new(f);
+
+    let mut words = Vec::with_capacity(12000);
+    let mut levels: Vec<String> = Vec::with_capacity(1400);
+    
+    for line in f.lines() {
+        let word = line.unwrap().to_lowercase();
+        if !badwords.contains(&word) {
+            let mut a: Vec<u8> = word.bytes().collect();
+            a.sort();
+            let sorted = String::from_utf8(a).unwrap();
+
+            if word.len() == 6 {
+                if !levels.iter().any(|x| x == &sorted) {
+                    levels.push(sorted.clone());
+                }
+            }
+
+            let wd = WordData { word: word, sorted: sorted };
+            words.push(wd);
+        }
+    }
+
+    if !dryrun {
+        fs::create_dir_all(&levelsdir).unwrap();
+    }
+
+    let mut level_counter = 0;
+
+    for bigword in levels.into_iter() {
+        let level_words =
+            words.iter().filter_map(
+                |x| -> Option<String> {
+                    if is_subgram(&x.sorted, &bigword) {
+                        Some(x.word.clone())
+                    } else {
+                        None
+                    }
+                }).collect::<Vec<String>>();
+
+        if level_words.len() >= min_words {
+            if !dryrun {
+                let mut f = File::create(
+                        Path::new(&levelsdir)
+                        .join(format!("{:04}.txt", level_counter))).unwrap();
+
+                for w in level_words.into_iter() {
+                    write!(f, "{}\n", w).unwrap();
+                }
+            }
+
+            level_counter += 1;
+        }
+    }
+
+    println!("var LEVELS_COUNT={};", level_counter);
+}
